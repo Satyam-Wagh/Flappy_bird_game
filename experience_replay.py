aa89625a3d965708e380bce3a79298be468e9368
Satyam-Wagh <satyamwagh2006@gmail.com>
2026-07-28 18:17:38 +0530

    Fix typos and import issues in experience replay

--- experience_replay.py
+++ experience_replay.py
@@ -1,16 +1,16 @@
-from collection import deque
+from collections import deque
 import random
 
 class ReplayMemmory():
     #creat the FIFO queue=>experience replay
-    def __init__(self,mexlen,seed=None):
+    def __init__(self,maxlen,seed=None):
         self.memory=deque([],maxlen=maxlen)
         
     def append(self,new_exp):
-        self.memory.appen(new_exp)
+        self.memory.append(new_exp)
         
     def sample(self,sample_size):
         return random.sample(self.memory,sample_size)
     #above code is say that we want the randome sample from memmory of "sample_size"
-    def __len__():
+    def __len__(self):
         return len(self.memory)
