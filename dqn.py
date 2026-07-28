1e5b3df219892f869a9d2c3af40752bfb1842091
Satyam-Wagh <satyamwagh2006@gmail.com>
2026-07-28 18:16:33 +0530

    Fix import and typo in DQN class definition

--- dqn.py
+++ dqn.py
@@ -1,12 +1,11 @@
 import torch
-import torch.nn
-
+import torch.nn as nn
 class DQN(nn.Module):
     def __init__(self,state_dim=12,action_dim=2,hidden_dim=256):
         #input dim,output dim,hidden dim
         super(DQN,self).__init__()
         
-        self.model=nn.Sequntial(
+        self.model=nn.Sequential(
             nn.Linear(state_dim,hidden_dim),
             nn.ReLU(),
             nn.Linear(hidden_dim,action_dim)
